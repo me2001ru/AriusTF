17bbd723f208cdb4eb1535bdeff25429f9e86559
Zephyro Kemstedt <zkemstedt@gmail.com>
2021-10-08 15:10:01 +0200

    normalize subnet variable names & subnet names

--- variables.tf
+++ variables.tf
@@ -1,64 +1,25 @@
-/*  ## VARIABLES
+## VARIABLES
+
 # Make changes here
 
-variable "network_name" {
-  type    = string
-  default = "Main-Network"
-}
-#First subnet details
-variable "subnet_name" {
-  type    = string
-  default = "Network_subnet_1"
-}
-variable "subnet_cidr" {
-  type    = string
-  default = "192.168.1.0/24"
-}
-#Second subnet details
-variable "subnet_name_2" {
-  type    = string
-  default = "Network_subnet_1"
-}
-variable "subnet_cidr_2" {
-  type    = string
-  default = "192.168.2.0/24"
-}
-#Third subnet details
-variable "subnet_name_3" {
-  type    = string
-  default = "Network_subnet_1"
-}
-variable "subnet_cidr_3" {
-  type    = string
-  default = "192.168.3.0/24"
-}
-#Fourth subnet details
-variable "subnet_name_4" {
-  type    = string
-  default = "Network_subnet_1"
-}
-variable "subnet_cidr_4" {
-  type    = string
-  default = "192.168.4.0/24"
-}
+/*
 # router id
 variable "router_id" {
   type    = string
   default = "600b8501-78cb-4155-9c9f-23dfcba88828"
 }
-
-variable "dns_ip" {
-  type    = list(string)
-  default = ["8.8.8.8", "8.8.4.4"]
-}
 */
 
-# router id
+
+## NETWORK
+
+# Default External Gateway
 variable "external_gateway" {
   type    = string
   default = "600b8501-78cb-4155-9c9f-23dfcba88828"
 }
 
+# Networks
 variable "network_name_dmz" {
   type    = string
   default = "DMZ-network"
@@ -84,45 +45,45 @@ variable "network_name_wlan" {
   default = "WLAN-network"
 }
 
+#  DNS
 variable "dns_ip" {
   type    = list(string)
   default = ["8.8.8.8", "8.8.4.4"]
 }
 
-#First subnet details
-variable "dmz_subnet" {
+# Subnets
+variable "subnet_name_dmz" {
   type    = string
-  default = "Network_subnet_1"
+  default = "DMZ-subnet"
 }
 variable "subnet_cidr_dmz" {
   type    = string
   default = "192.168.1.0/24"
 }
 
-variable "media_subnet" {
+variable "subnet_name_media" {
   type    = string
-  default = "Network_subnet_1"
+  default = "Media-subnet"
 }
 variable "subnet_cidr_media" {
   type    = string
   default = "192.168.2.0/24"
 }
 
-variable "hosting_subnet" {
+variable "subnet_name_hosting" {
   type    = string
-  default = "Network_subnet_1"
+  default = "Hosting-subnet"
 }
 variable "subnet_cidr_hosting" {
   type    = string
   default = "192.168.3.0/24"
 }
 
-variable "departments_subnet" {
+variable "subnet_name_departments" {
   type    = string
-  default = "Network_subnet_1"
+  default = "Departments-subnet"
 }
 variable "subnet_cidr_departments" {
   type    = string
   default = "192.168.4.0/24"
 }
-
