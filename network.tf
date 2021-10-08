1fad65e1f681a53a3c7eb19eaf4780e4a430a364
hallonpie <kazisadman.rashid@yh.nackademin.se>
2021-10-08 23:16:20 +0200

    subnet done in wlan

--- network.tf
+++ network.tf
@@ -1,82 +1,118 @@
 # Create network
-resource "openstack_networking_network_v2" "DMZ" {
+resource "openstack_networking_network_v2" "network_dmz" {
   name           = var.network_name_dmz
   admin_state_up = "true"
 }
 
-resource "openstack_networking_network_v2" "Media" {
+resource "openstack_networking_network_v2" "network_media" {
   name           = var.network_name_media
   admin_state_up = "true"
 }
 
-resource "openstack_networking_network_v2" "Hosting" {
+resource "openstack_networking_network_v2" "network_hosting" {
   name           = var.network_name_hosting
   admin_state_up = "true"
 }
 
-resource "openstack_networking_network_v2" "Departments" {
+resource "openstack_networking_network_v2" "network_department" {
   name           = var.network_name_department
   admin_state_up = "true"
 }
 
-resource "openstack_networking_network_v2" "Wlan" {
+resource "openstack_networking_network_v2" "network_wlan" {
   name           = var.network_name_wlan
   admin_state_up = "true"
 }
 
-# Create subnets
-resource "openstack_networking_subnet_v2" "dmz_subnet" {
-  name            = var.dmz_subnet
-  network_id      = openstack_networking_network_v2.DMZ.id
+# Create Subnets
+resource "openstack_networking_subnet_v2" "subnet_dmz" {
+  name            = var.subnet_name_dmz
+  network_id      = openstack_networking_network_v2.network_dmz.id
   cidr            = var.subnet_cidr_dmz
   ip_version      = 4
   dns_nameservers = var.dns_ip
 }
 
-#routers first interface connected to dmz_subnet
-resource "openstack_networking_router_interface_v2" "router_interface_dmz" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.dmz_subnet.id
-}
-
-resource "openstack_networking_subnet_v2" "media_subnet" {
-  name            = var.media_subnet
-  network_id      = openstack_networking_network_v2.Media.id
+resource "openstack_networking_subnet_v2" "subnet_media" {
+  name            = var.subnet_name_media
+  network_id      = openstack_networking_network_v2.network_media.id
   cidr            = var.subnet_cidr_media
   ip_version      = 4
   dns_nameservers = var.dns_ip
 }
 
-#routers first interface connected to dmz_subnet
-resource "openstack_networking_router_interface_v2" "router_interface_media" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.media_subnet.id
-}
-
-resource "openstack_networking_subnet_v2" "hosting_subnet" {
-  name            = var.hosting_subnet
-  network_id      = openstack_networking_network_v2.Hosting.id
+resource "openstack_networking_subnet_v2" "subnet_hosting" {
+  name            = var.subnet_name_hosting
+  network_id      = openstack_networking_network_v2.network_hosting.id
   cidr            = var.subnet_cidr_hosting
   ip_version      = 4
   dns_nameservers = var.dns_ip
 }
 
-#routers first interface connected to dmz_subnet
-resource "openstack_networking_router_interface_v2" "router_interface_hosting" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.hosting_subnet.id
-}
-
-resource "openstack_networking_subnet_v2" "departments_subnet" {
-  name            = var.departments_subnet
-  network_id      = openstack_networking_network_v2.Departments.id
+resource "openstack_networking_subnet_v2" "subnet_departments" {
+  name            = var.subnet_name_departments
+  network_id      = openstack_networking_network_v2.network_department.id
   cidr            = var.subnet_cidr_departments
   ip_version      = 4
   dns_nameservers = var.dns_ip
 }
 
-#routers first interface connected to dmz_subnet
+resource "openstack_networking_subnet_v2" "subnet_wlan_media" {
+  name            = var.subnet_name_wlan_media
+  network_id      = openstack_networking_network_v2.network_wlan.id
+  cidr            = var.subnet_cidr_wlan_media
+  ip_version      = 4
+  dns_nameservers = var.dns_ip
+}
+
+resource "openstack_networking_subnet_v2" "subnet_wlan_hosting" {
+  name            = var.subnet_name_wlan_hosting
+  network_id      = openstack_networking_network_v2.network_wlan.id
+  cidr            = var.subnet_cidr_wlan_hosting
+  ip_version      = 4
+  dns_nameservers = var.dns_ip
+}
+
+resource "openstack_networking_subnet_v2" "subnet_wlan_department" {
+  name            = var.subnet_name_wlan_department
+  network_id      = openstack_networking_network_v2.network_wlan.id
+  cidr            = var.subnet_cidr_wlan_department
+  ip_version      = 4
+  dns_nameservers = var.dns_ip
+}
+
+# Create Interfaces
+resource "openstack_networking_router_interface_v2" "router_interface_dmz" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_dmz.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_media" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_media.id
+}
+
 resource "openstack_networking_router_interface_v2" "router_interface_departments" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.departments_subnet.id
+    router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_departments.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_hosting" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_hosting.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_wlan_media" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_wlan_media.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_wlan_hosting" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_wlan_hosting.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_wlan_departments" {
+    router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet_wlan_department.id
 }
