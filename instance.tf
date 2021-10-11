bff2ae2181e80021e32908a31276291202178c8e
me2001ru <mehsum.rupani@yh.nackademin.se>
2021-10-11 16:02:15 +0200

    minor changes to it_admin and webserver instances

--- instance.tf
+++ instance.tf
@@ -98,8 +98,7 @@ resource "openstack_compute_instance_v2" "instance_IT" {
   user_data       = var.cloudconfig_it_admin
 
   network {
-    port       = openstack_networking_port_v2.port_ip_it_admin.id
-    ip_address = var.port_ip_it_admin
+    port = openstack_networking_port_v2.port_ip_it_admin.id
   }
 }
 
