5edeb939168f18b353465c1a25b41f36304b8648
Zephyro Kemstedt <zkemstedt@gmail.com>
2021-10-11 15:30:13 +0200

    redo things ?

--- main.tf
+++ main.tf
@@ -8,39 +8,3 @@ provider "openstack" {
   #auth_url           = "" # use $OS_AUTH_URL
   #region             = "" # use $OS_REGION_NAME
 }
-
-
-#Set port
-
-#Set Security-group
-resource "openstack_compute_secgroup_v2" "secgroup_1" {
-  name        = "TerraformHTTP(S)"
-  description = "Set Port http and https"
-
-  rule {
-    from_port   = 80
-    to_port     = 80
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = 443
-    to_port     = 443
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-}
-
-resource "openstack_compute_secgroup_v2" "secgroup_2" {
-  name        = "TerraformSSH"
-  description = "SSH "
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0" # change this to appropriate allowed ip's.
-  }
-}
-
