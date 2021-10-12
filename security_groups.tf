0400cab70880f0d298e5c92cb0b9ef2d1c7f5e81
Zephyro Kemstedt <zkemstedt@gmail.com>
2021-10-12 11:01:26 +0200

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -2,7 +2,7 @@
 
 # Webserver
 resource "openstack_compute_secgroup_v2" "webserver" {
-  name        = "Webserver: HTTP/HTTPS"
+  name        = "Webserver"
   description = "Allow http/https traffic to WebServer"
 
   rule {
@@ -59,21 +59,48 @@ resource "openstack_compute_secgroup_v2" "MySQL" {
   }
 }
 
-# Allow connection to NextCloud server 
-resource "openstack_compute_secgroup_v2" "NextCloud" {
+# NextCloud
+resource "openstack_networking_secgroup_v2" "NextCloud" {
   name        = "NextCloud"
   description = "Allow tcp access from Wlan Subnet"
+}
 
-  rule {
-    from_port   = 443
-    to_port     = 443
-    ip_protocol = "tcp"
-    cidr        = "192.168.10.0/24"
-  }
-  rule {
-    from_port   = 80
-    to_port     = 80
-    ip_protocol = "tcp"
-    cidr        = "192.168.10.0/24"
-  }
+# Http in
+resource "openstack_networking_secgroup_rule_v2" "NextCloud_Rule_Inbound_HTTP" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = "${openstack_networking_secgroup_v2.NextCloud.id}"
+}
+
+# Https in
+resource "openstack_networking_secgroup_rule_v2" "NextCloud_Rule_Inbound_HTTPS" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = "${openstack_networking_secgroup_v2.NextCloud.id}"
+}
+
+# Http out
+resource "openstack_networking_secgroup_rule_v2" "NextCloud_Rule_Outbound_HTTP" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = "${openstack_networking_secgroup_v2.NextCloud.id}"
+}
+
+# Https out
+resource "openstack_networking_secgroup_rule_v2" "NextCloud_Rule_Outbound_HTTPS" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = "${openstack_networking_secgroup_v2.NextCloud.id}"
 }
